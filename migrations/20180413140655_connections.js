b0f761f3833fab0acbd173301f782576aedc8f80
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-13 15:42:17 -0500

    Fix to Connections migration

--- migrations/20180413140655_connections.js
+++ migrations/20180413140655_connections.js
@@ -1,10 +1,12 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('connections', table => {
 		table.increments();
+		table.integer('user_id_owner');
 		table
 			.foreign('user_id_owner')
 			.references('users.id')
 			.onDelete('CASCADE');
+		table.integer('user_id_friend');
 		table
 			.foreign('user_id_friend')
 			.references('users.id')
