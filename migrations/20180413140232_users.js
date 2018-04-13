781e26d48e58335f35bc409257bb7a85c9381631
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-13 14:10:23 -0500

    Users initial migration file

--- migrations/20180413140232_users.js
+++ migrations/20180413140232_users.js
@@ -1,8 +1,19 @@
-
 exports.up = function(knex, Promise) {
-  
+	return knex.schema.createTable('users', table => {
+		table.increments();
+		table.string('email');
+		table.string('location');
+		table.string('interests');
+		table.string('hashed_pw');
+		table.string('email_personal');
+		table.string('email_professional');
+		table.string('linkedin');
+		table.string('facebook');
+		table.string('phone');
+		table.timestamps(true, true);
+	});
 };
 
 exports.down = function(knex, Promise) {
-  
+	return knex.schema.dropTable('users');
 };
