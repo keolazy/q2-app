01fb19d5c3c15370e0ac84573f24c2379614323d
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-13 14:06:24 -0500

    Initial knexfile

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+module.exports = {
+	development: {
+		client: 'pg',
+		connection: {
+			database: 'readme-app-dev'
+		}
+	},
+
+	development: {
+		client: 'pg',
+		connection: {
+			database: 'readme-app-test'
+		}
+	}
+};
