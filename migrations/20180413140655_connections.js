93d322361f3ba64cc941ac52dadc7068f66fcc62
Nathan Keolasy <keolazy1@gmail.com>
2018-04-13 14:21:46 -0500

    added connections table migration

---
+++ migrations/20180413140655_connections.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('connections', (table) => {
+    table.increments();
+    table.foreign('user_id_owner').references('users.id').onDelete('CASCADE');
+    table.foreign('user_id_friend').references('users.id').onDelete('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('connections');
+  })
+};
