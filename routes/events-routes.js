6ece0342be0507c5fa280ff8be9052b998cc3d82
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-14 13:02:10 -0500

    First couple 'events' tests

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.status(200).json(`This is the '/' route.`);
+	res.status(200).json(`The events are here.`);
 });
 
 module.exports = router;
