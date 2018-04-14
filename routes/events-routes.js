3bf5c4d9ed7c8323edf58c8b8faeb5d623f0b62b
Kevin Galvan <klgalvan79@gmail.com>
2018-04-14 13:18:04 -0500

    route get events

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../db/knex');
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended:true}));
 
-router.get('/', (req, res, next) => {
-	res.status(200).json(`The events are here.`);
+router.get('/', (req, res) => {
+	// res.status(200).json(`The events are here.`)
+		knex('events').select('*')
+		.then( (events) => {
+			res.send(events)
+			console.log('sounds good nigga')
+		})
 });
 
 module.exports = router;
