2ec60ddfc0aac44d41f162e22437d5cb969b458e
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-14 11:33:23 -0500

    Fix merge junk on knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,17 @@
 // Update with your config settings.
 
 module.exports = {
-  development: {
-    client: "postgresql",
-    connection: {
-      database: "readme-app-dev"
-    }
-  },
+	development: {
+		client: 'postgresql',
+		connection: {
+			database: 'readme-app-dev'
+		}
+	},
 
-<<<<<<< HEAD
-  test: {
-    client: "postgresql",
-    connection: {
-      database: "readme-app-test"
-    }
-  }
-=======
 	test: {
 		client: 'postgresql',
 		connection: {
 			database: 'readme-app-test'
 		}
 	}
->>>>>>> fa1500e7e45cd7192b54254d317c170db9901e7e
 };
