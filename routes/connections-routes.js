7021a96b656624f19d4320640abcee1b18410bb5
Nathan Keolasy <keolazy1@gmail.com>
2018-04-14 16:27:19 -0500

    routes/connections updated

--- routes/connections-routes.js
+++ routes/connections-routes.js
@@ -1,8 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const knex = require("../db/knex");
+const bodyParser = require("body-parser");
 
-router.get('/', (req, res, next) => {
-	res.status(200).json(`This is the '/' route.`);
+router.get("/", (req, res) => {
+  knex("connections")
+    .select("*")
+    .then(connections => {
+      res.send(connections);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  knex("connections")
+    .where("id", req.params.id)
+    .then(connection => {
+      res.json(connection);
+    });
+});
+
+router.post("/", (req, res) => {
+  knex("connections")
+    .insert(req.body)
+    .then(() => {
+      res.redirect("/connections");
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  knex("connections")
+    .where("id", req.params.id) // returns array
+    .first()
+    .del()
+    .then(() => {
+      res.redirect("/connections");
+    });
 });
 
 module.exports = router;
--- seeds/2_connections.js
+++ seeds/2_connections.js
@@ -1,10 +1,11 @@
 exports.seed = function(knex, Promise) {
-	return knex('connections').insert([
-		{ user_id_owner: '2', user_id_friend: '4' },
-		{ user_id_owner: '1', user_id_friend: '3' },
-		{ user_id_owner: '1', user_id_friend: '6' },
-		{ user_id_owner: '3', user_id_friend: '8' },
-		{ user_id_owner: '2', user_id_friend: '4' },
-		{ user_id_owner: '7', user_id_friend: '8' }
-	]);
+  return knex("connections").insert([
+    { user_id_owner: "2", user_id_friend: "4" },
+    { user_id_owner: "4", user_id_friend: "2" },
+    { user_id_owner: "1", user_id_friend: "3" },
+    { user_id_owner: "1", user_id_friend: "6" },
+    { user_id_owner: "3", user_id_friend: "8" },
+    { user_id_owner: "2", user_id_friend: "4" },
+    { user_id_owner: "7", user_id_friend: "8" }
+  ]);
 };
