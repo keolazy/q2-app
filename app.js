27350889113297926ee5ae987f2f6843e01f3842
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-15 14:49:06 -0500

    Basic authentication working

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = process.env.PORT || 7777;
 const knex = require('./db/knex.js');
+const path = require('path');
 
 const events = require('./routes/events-routes');
 const connections = require('./routes/connections-routes');
 const profiles = require('./routes/profiles-routes');
+const auth = require('./controllers/auth');
 
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+	res.redirect('/login.html');
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/auth', auth);
 app.use('/events', events);
 app.use('/connections', connections);
 app.use('/profiles', profiles);
--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,28 @@ const bcrypt = require('bcrypt-as-promised');
 const router = require('express').Router();
 const knex = require('../db/knex');
 
-router.post('/', (req, res, next) => {});
+router.post('/', (req, res, next) => {
+	console.log(req.body);
+	let givenEmail = req.body.email;
+	let givenPw = req.body.password;
+	knex('users')
+		.where('email', givenEmail)
+		.first()
+		.then(userOnFile => {
+			// check for bcrypt match
+			bcrypt
+				.compare(givenPw, userOnFile.hashed_pw)
+				.then(success => {
+					res.status(200).json('This is the right password!');
+				})
+				.catch(failure => {
+					res.status(400).json('Wrong password');
+				});
+		})
+		.catch(err => {
+			res.status(400).json('That email is not on file');
+		});
+	// res.status(200).json('All clear here');
+});
 
 module.exports = router;
