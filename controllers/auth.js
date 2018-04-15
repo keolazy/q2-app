97eaf95fe6e3a631c851ca07f66d1fc2458a8a93
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-15 14:11:10 -0500

    Started auth file

---
+++ controllers/auth.js
@@ -0,0 +1,7 @@
+const bcrypt = require('bcrypt-as-promised');
+const router = require('express').Router();
+const knex = require('../db/knex');
+
+router.post('/', (req, res, next) => {});
+
+module.exports = router;
