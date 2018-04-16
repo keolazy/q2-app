c53329ec99d6fbaef61fc6ed46cbd2f742b04cb8
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-16 12:42:01 -0500

    Profiles per-event working

--- routes/profiles-routes.js
+++ routes/profiles-routes.js
@@ -5,10 +5,23 @@ const knex = require('../db/knex');
 router.get('/', (req, res, next) => {
 	knex('users_events')
 		.where('event_id', req.params.id)
+		.innerJoin('users', 'users_events.user_id', 'users.id')
+		.select(
+			'users.id',
+			'users.first',
+			'users.last',
+			'users_events.questions',
+			'users_events.topics',
+			'users_events.job_status',
+			'users_events.noise_level',
+			'users_events.where_to_find',
+			'users_events.ask_me',
+			'users_events.personality'
+		)
 		.then(response => {
-			res.status(200).json(response);
+			// res.status(200).json(response);
+			res.render('profiles/all', { profiles: response });
 		});
-	// res.status(200).send(`You're asking for profile for event ID: ${req.params.id}`);
 });
 
 module.exports = router;
