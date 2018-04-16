099fb2947b00a5bcf3acad531fc374cce555c0fe
Kevin Galvan <klgalvan79@gmail.com>
2018-04-16 11:38:55 -0500

    added profile route

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const profiles = require('./profiles-routes');
+
 // const bodyParser = require('body-parser');
 // router.use(bodyParser.json());
 // router.use(bodyParser.urlencoded({extended:true}));
+
+router.use('/profiles', profiles)
+
+router.get('/', (req,res,next) => {
+    if (req.session.user) {
+        next()
+    }  else {
+        res.redirect('/login.html')
+    }
+}
 
 router.get('/', (req, res) => {
 	// res.status(200).json(`The events are here.`)
@@ -12,14 +24,34 @@ router.get('/', (req, res) => {
 			res.send(events)
 		})
 });
+
 router.get('/:id', (req, res) => {
 	knex('events').select('*')
 	.where({id:req.params.id}).then( (oneEvent) => {
 		res.send(oneEvent)
-		console.log(oneEvent[0].location)
 	})
 })
-// router.post('/', (req, res) => {
-//
-// })
+
+router.post('/', (req, res) => {
+	knex('events').insert({name:req.query.name, description:req.query.description, location:req.query.location,
+	date:req.query.date, start_time:req.query.start_time, end_time:req.query.start_time,
+	host_id:req.query.host_id}).then( () => {
+		res.send('its party time')
+	})
+})
+router.patch('/:id', (req, res) => {
+	knex('events').where({id:req.params.id}).update({ name:req.query.name, description:req.query.description,
+		location:req.query.location, date:req.query.date, start_time:req.query.start_time,
+		end_time:req.query.start_time, host_id:req.query.host_id})
+		.then( () => {
+		res.send('dang fool')
+		})
+})
+router.delete('/:id', (req, res) => {
+	knex('events').where({id:req.params.id}).del().then( () => {
+		res.send('party times over');
+	})
+})
+
+
 module.exports = router;
