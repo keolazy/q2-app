d05f2320b95fee1d63b437feacde9e5d31f50064
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-16 11:43:03 -0500

    Fix to event-routes

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -7,51 +7,72 @@ const profiles = require('./profiles-routes');
 // router.use(bodyParser.json());
 // router.use(bodyParser.urlencoded({extended:true}));
 
-router.use('/profiles', profiles)
+router.use('/profiles', profiles);
 
-router.get('/', (req,res,next) => {
-    if (req.session.user) {
-        next()
-    }  else {
-        res.redirect('/login.html')
-    }
-}
+router.get('/', (req, res, next) => {
+	if (req.session.user) {
+		next();
+	} else {
+		res.redirect('/login.html');
+	}
+});
 
 router.get('/', (req, res) => {
 	// res.status(200).json(`The events are here.`)
-		knex('events').select('*')
-		.then( (events) => {
-			res.send(events)
-		})
+	knex('events')
+		.select('*')
+		.then(events => {
+			res.send(events);
+		});
 });
 
 router.get('/:id', (req, res) => {
-	knex('events').select('*')
-	.where({id:req.params.id}).then( (oneEvent) => {
-		res.send(oneEvent)
-	})
-})
+	knex('events')
+		.select('*')
+		.where({ id: req.params.id })
+		.then(oneEvent => {
+			res.send(oneEvent);
+		});
+});
 
 router.post('/', (req, res) => {
-	knex('events').insert({name:req.query.name, description:req.query.description, location:req.query.location,
-	date:req.query.date, start_time:req.query.start_time, end_time:req.query.start_time,
-	host_id:req.query.host_id}).then( () => {
-		res.send('its party time')
-	})
-})
-router.patch('/:id', (req, res) => {
-	knex('events').where({id:req.params.id}).update({ name:req.query.name, description:req.query.description,
-		location:req.query.location, date:req.query.date, start_time:req.query.start_time,
-		end_time:req.query.start_time, host_id:req.query.host_id})
-		.then( () => {
-		res.send('dang fool')
+	knex('events')
+		.insert({
+			name: req.query.name,
+			description: req.query.description,
+			location: req.query.location,
+			date: req.query.date,
+			start_time: req.query.start_time,
+			end_time: req.query.start_time,
+			host_id: req.query.host_id
 		})
-})
+		.then(() => {
+			res.send('its party time');
+		});
+});
+router.patch('/:id', (req, res) => {
+	knex('events')
+		.where({ id: req.params.id })
+		.update({
+			name: req.query.name,
+			description: req.query.description,
+			location: req.query.location,
+			date: req.query.date,
+			start_time: req.query.start_time,
+			end_time: req.query.start_time,
+			host_id: req.query.host_id
+		})
+		.then(() => {
+			res.send('dang fool');
+		});
+});
 router.delete('/:id', (req, res) => {
-	knex('events').where({id:req.params.id}).del().then( () => {
-		res.send('party times over');
-	})
-})
-
+	knex('events')
+		.where({ id: req.params.id })
+		.del()
+		.then(() => {
+			res.send('party times over');
+		});
+});
 
 module.exports = router;
