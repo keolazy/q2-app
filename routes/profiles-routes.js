ab9bfeb41c0bab245fce442e358bfdec902f0149
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-16 15:48:11 -0500

    Profile pages

--- routes/profiles-routes.js
+++ routes/profiles-routes.js
@@ -19,6 +19,7 @@ router.get('/', (req, res, next) => {
 			'users.id',
 			'users.first',
 			'users.last',
+			'users_events.id',
 			'users_events.questions',
 			'users_events.topics',
 			'users_events.job_status',
@@ -56,33 +57,44 @@ router.get('/', (req, res, next) => {
 			console.log(err);
 			res.status(500).send(err);
 		});
+});
 
-	// knex('users_events')
-	// 	.where('event_id', req.params.id)
-	// 	.innerJoin('users', 'users_events.user_id', 'users.id')
-	// 	.select(
-	// 		'users.id',
-	// 		'users.first',
-	// 		'users.last',
-	// 		'users_events.questions',
-	// 		'users_events.topics',
-	// 		'users_events.job_status',
-	// 		'users_events.noise_level',
-	// 		'users_events.where_to_find',
-	// 		'users_events.ask_me',
-	// 		'users_events.personality'
-	// 	)
-	// 	.then(profiles => {
-	// 		knex('events')
-	// 			.where('id', req.params.id)
-	// 			.first()
-	// 			.then(event => {
-	// 				console.log(`Profiles: ` + JSON.stringify(profiles));
-	// 				console.log(`Event: ` + JSON.stringify(event));
-	// 				res.render('profiles/all', { event: event, profiles: profiles });
-	// 			});
-	// res.status(200).json(profiles);
-	// });
+router.get('/:profileID', (req, res, next) => {
+	let theProfile = knex('users_events')
+		.where('id', req.params.profileID)
+		.innerJoin('users', 'users_events.user_id', 'users.id')
+		.select(
+			'users.id',
+			'users.first',
+			'users.last',
+			'users_events.id',
+			'users_events.questions',
+			'users_events.topics',
+			'users_events.job_status',
+			'users_events.noise_level',
+			'users_events.where_to_find',
+			'users_events.ask_me',
+			'users_events.personality'
+		);
+
+	let theEvent = knex('events').where('id', req.params.id);
+
+	let theConnections = [];
+
+	if (req.session.user) {
+		theConnections = knex('connections').where('user_id_owner', req.session.user);
+	}
+
+	Promise.all([theProfiles, theEvent, theConnections]).then(data => {
+		let profileData = data[0];
+		let eventData = data[1];
+		let connectionData = data[2];
+		res.render('profiles/single', {
+			event: eventData,
+			profiles: profileData,
+			connections: connectionData
+		});
+	});
 });
 
 module.exports = router;
