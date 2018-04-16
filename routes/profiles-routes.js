0db35c3a4340751917791fd37970a39187ec2494
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-16 13:44:59 -0500

    Progress on profiles view

--- routes/profiles-routes.js
+++ routes/profiles-routes.js
@@ -3,6 +3,28 @@ const router = express.Router({ mergeParams: true });
 const knex = require('../db/knex');
 
 router.get('/', (req, res, next) => {
+	// let profiles = knex('users_events')
+	// 	.where('event_id', req.params.id)
+	// 	.innerJoin('users', 'users_events.user_id', 'users.id')
+	// 	.select(
+	// 		'users.id',
+	// 		'users.first',
+	// 		'users.last',
+	// 		'users_events.questions',
+	// 		'users_events.topics',
+	// 		'users_events.job_status',
+	// 		'users_events.noise_level',
+	// 		'users_events.where_to_find',
+	// 		'users_events.ask_me',
+	// 		'users_events.personality'
+	// 	);
+	//
+	// let event = knex('events')
+	// 	.where('id', req.params.id)
+	// 	.first();
+	//
+	// let connections = knex('connections')
+
 	knex('users_events')
 		.where('event_id', req.params.id)
 		.innerJoin('users', 'users_events.user_id', 'users.id')
@@ -18,9 +40,14 @@ router.get('/', (req, res, next) => {
 			'users_events.ask_me',
 			'users_events.personality'
 		)
-		.then(response => {
-			// res.status(200).json(response);
-			res.render('profiles/all', { profiles: response });
+		.then(profiles => {
+			knex('events')
+				.where('id', req.params.id)
+				.first()
+				.then(event => {
+					res.render('profiles/all', { event: event, profiles: profiles });
+				});
+			// res.status(200).json(profiles);
 		});
 });
 
