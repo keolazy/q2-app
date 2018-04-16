a27656a546a9726001b56511c25f46d3a197cbb7
Kevin Galvan <klgalvan79@gmail.com>
2018-04-16 15:14:28 -0500

    editEvent, new event

--- routes/events-routes.js
+++ routes/events-routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const knex = require('../db/knex');
 const profiles = require('./profiles-routes');
-
+const methodOverride = require('method-override');
 // const bodyParser = require('body-parser');
 // router.use(bodyParser.json());
 // router.use(bodyParser.urlencoded({extended:true}));
+
 
 router.get('/', (req, res, next) => {
 	if (req.session.user) {
@@ -33,34 +34,66 @@ router.get('/:id', (req, res) => {
 		});
 });
 
+router.get('/:id/edit', (req, res) => {
+	knex('events')
+		.select('*')
+		.where({ id: req.params.id })
+		.then(oneEvent => {
+			res.render('../views/editEvent', {oneEvent:oneEvent})
+			console.log(oneEvent)
+		});
+});
+
+router.put('/:id/edit', (req, res) => {
+	router.use(methodOverride('application/x-www-form-urlencoded'))
+	knex('events')
+		.where({ id: req.params.id })
+		.update({
+			name: req.body.name,
+			description: req.body.description,
+			location: req.body.location,
+			date: req.body.date,
+			start_time: req.body.start_time,
+			end_time: req.body.end_time,
+			host_id: req.body.host_id
+		})
+		.then(() => {
+			res.send('you just updated the event');
+			console.log(req.body.name)
+		});
+})
+
+
+
+
 router.use('/:id/profiles', profiles);
 
 router.post('/', (req, res) => {
 	knex('events')
 		.insert({
-			name: req.query.name,
-			description: req.query.description,
-			location: req.query.location,
-			date: req.query.date,
-			start_time: req.query.start_time,
-			end_time: req.query.start_time,
-			host_id: req.query.host_id
+			name: req.body.name,
+			description: req.body.description,
+			location: req.body.location,
+			date: req.body.date,
+			start_time: req.body.start_time,
+			end_time: req.body.start_time,
+			host_id: req.body.host_id
 		})
-		.then(() => {
-			res.send('its party time');
+		.then((result) => {
+			res.send(result);
 		});
 });
 router.patch('/:id', (req, res) => {
 	knex('events')
 		.where({ id: req.params.id })
 		.update({
-			name: req.query.name,
-			description: req.query.description,
-			location: req.query.location,
-			date: req.query.date,
-			start_time: req.query.start_time,
-			end_time: req.query.start_time,
-			host_id: req.query.host_id
+			name: req.body.name,
+			description: req.body.description,
+			location: req.body.location,
+			date: req.body.date,
+			start_time: req.body.start_time,
+			end_time: req.body.start_time,
+			host_id: req.body.host_id
 		})
 		.then(() => {
 			res.send('dang fool');
