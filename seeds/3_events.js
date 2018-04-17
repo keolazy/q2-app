028619621cd49b29c8101043b241d7cf4ce37c81
Ian Schumann <ian.l.schumann@gmail.com>
2018-04-17 16:06:37 -0500

    Finally fixed dem seed files :-)

--- seeds/3_events.js
+++ seeds/3_events.js
@@ -1,39 +1,41 @@
 exports.seed = function(knex, Promise) {
 	return knex('events').insert([
 		{
-			name: 'Christmas Fiasco',
-			description: 'See Lupe Fiasco perfom on christmas',
-			location: 'Auditorium Shores',
-			date: '2018-12-25',
-			start_time: '15',
-			end_time: '23',
+			name: 'A Night of Python Coding',
+			description:
+				'Galvanize will be hosting Python coders on the third Tuesday every month. Bring your laptops and join the coding.',
+			location: 'Galvanize Campus',
+			date: '2018-10-25',
+			start_time: '18:30',
+			end_time: '21:00',
 			host_id: '4'
 		},
 		{
-			name: 'Blood Feast',
-			description: 'see live cannibals perfom in July',
-			location: 'Barton Springs',
+			name: 'Machine Learning and AI Q&A Session',
+			description: `Know ML or Math? Want to give back? We need more mentors/teachers. :)
+Before attending, please read this entire website >> http://CppMSG.com << especially the meeting protocols and rules page since we are unique/unusual. ;)`,
+			location: 'ATX Hackerspace Co-op',
 			date: '2018-7-13',
-			start_time: '10',
-			end_time: '19',
+			start_time: '19:30',
+			end_time: '21:30',
 			host_id: '2'
 		},
 		{
-			name: "Eeyore's Birthday Party",
-			description: 'pointless celebration',
-			location: 'Peace District Park',
+			name: 'Applications, Interviews, and Everything In-Between: A Career Bootcamp',
+			description: `We’ll spend the day approaching the job search from a practical and bite-sized perspective.`,
+			location: 'General Assembly Austin',
 			date: '2018-4-17',
-			start_time: '7',
-			end_time: '23',
+			start_time: '18:00',
+			end_time: '21:00',
 			host_id: '1'
 		},
 		{
-			name: 'Borat Impression Contest',
-			description: 'Celebrate Graduation',
-			location: 'PowerPlant',
+			name: 'Austin Digital Jobs Mixer',
+			description: `This is the highly anticipated Austin Digital Jobs Group analog event, bringing the city's most desirable employers and qualified candidates together over a beer. `,
+			location: 'Rattle Inn',
 			date: '2018-6-1',
-			start_time: '20',
-			end_time: '3',
+			start_time: '18:45',
+			end_time: '22:00',
 			host_id: '5'
 		}
 	]);
