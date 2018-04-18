2ca2c8c911369a4976635fe88a0c163d02a1c21a
Kevin Galvan <klgalvan79@gmail.com>
2018-04-18 10:15:40 -0500

    fixed personal connection conflict

--- public/shared_scripts.js
+++ public/shared_scripts.js
@@ -4,10 +4,10 @@ let navOpen = document.getElementById('hamburger-menu');
 let navUL = document.getElementById('nav__ul');
 
 navUL.innerHTML = `
-<li class="nav__ul__li">Home</li>
-<li class="nav__ul__li">My Account</li>
-<li class="nav__ul__li">Events</li>
-<li class="nav__ul__li">Connections</li>
+<li class="nav__ul__li"><a href='/events'>Home</a></li>
+<li class="nav__ul__li"><a href='/account'>My Account</a></li>
+<li class="nav__ul__li"><a href='/events/all'>Events</li>
+<li class="nav__ul__li"><a href='/connections'>Connections</li>
 <li class="nav__ul__li">Logout</li>
 `;
 
