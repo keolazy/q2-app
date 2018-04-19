7dc23e6116adebc54df90b0fc07283e1711c6182
Kevin Galvan <klgalvan79@gmail.com>
2018-04-19 16:15:46 -0500

    new lab

--- routes/lab-routes.js
+++ routes/lab-routes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router({ mergeParams: true });
 const knex = require('../db/knex');
 const methodOverride = require('method-override');
+const cors = require('cors');
+const serveStatic = require('serveStatic');
 
 
+
+// router.get('https://www.eventbriteapi.com/v3/users/me/?token=R35L6HN4MS7M53LVFZN2', (req, res) => {
+//   res.send('done')
+// })
 
 module.exports = router;
