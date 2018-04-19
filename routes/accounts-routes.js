7517b9071c61c1bf1ec32d94aa7c849dbf8cd4b2
Nathan Keolasy <keolazy1@gmail.com>
2018-04-19 09:38:41 -0500

    account view+edit both work. edit form submits. doesn't redirect once updated though.

--- routes/accounts-routes.js
+++ routes/accounts-routes.js
@@ -24,6 +24,7 @@ router.get("/edit", (req, res) => {
     .where("id", req.session.user)
     .first()
     .then(data => {
+      console.log({ user: data });
       res.render("account/edit", { user: data });
     })
     .catch(error => {
@@ -31,8 +32,25 @@ router.get("/edit", (req, res) => {
     });
 });
 
-// router.put("/edit", (req, res) => {
-//   knex("events").where();
-// });
+router.put("/edit", (req, res) => {
+  knex("users")
+    .where({ id: req.session.user })
+    .first()
+    .then(result => {
+      console.log(`Result host ID is: ${result.id}`);
+      console.log(`Res.locals.user is: ${res.locals.user}`);
+      if (result.id == res.locals.user) {
+        knex("users")
+          .where({ id: req.session.user })
+          .update(req.body)
+          .then(result => {
+            console.log(`Account Succesfully Updated`);
+            res.redirect(`/account/view`);
+          });
+      } else {
+        res.status(400).json("you have to be the host to edit this account.");
+      }
+    });
+});
 
 module.exports = router;
